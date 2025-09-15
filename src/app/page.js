fc6571f031d4c802225ae7347457994c826d364d
MUSHUBO <shubo3132@gmail.com>
2025-09-16 04:22:13 +0600

    New Branch Created

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="text-4xl my-52 text-center">
-      <h1>Edu_Smart</h1>
+      <h1 className="text-blue-500">Edu_Smart</h1>
       <button className="btn btn-primary mt-4">Lets Start</button>
     </div>
   );
