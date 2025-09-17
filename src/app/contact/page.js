b6e0ed3fbcb26b4373f02817f58f479d9e41f47e
riy8n <mahafujhossainriyan02@gmail.com>
2025-09-17 23:40:15 +0600

    Aded the Student from update

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,11 +1,14 @@
-import ContactInfo from "../components/ContactInfo";
+import ContactInfo from "@/components/ContactInfo";
+import StudentFrom from "@/components/StudentFrom";
+
 
 
 export default function ContactPage() {
   return (
-    <section className="bg-accent py-12 px-4 md:px-10">
+    <section className="bg-background py-12 px-4 md:px-10">
       <div className="max-w-6xl mx-auto">
-        <ContactInfo />
+        <ContactInfo></ContactInfo>
+        <StudentFrom></StudentFrom>
         
       </div>
     </section>
