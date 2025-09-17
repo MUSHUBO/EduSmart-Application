ea7d0d1b4cf4df9fa9194740327bfcca7eb082d0
riy8n <mahafujhossainriyan02@gmail.com>
2025-09-17 23:58:18 +0600

    Added the api section

--- src/app/api/contact/route.js
+++ src/app/api/contact/route.js
@@ -4,7 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(req) {
   const data = await req.json();
 
-  console.log("Form Data:", data);
+ 
 
   return NextResponse.json({ success: true, message: "Form received!" });
 }
