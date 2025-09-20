5e563e6ae9024765ec7702dd33f06a4831d2aa17
MUSHUBO <shubo3132@gmail.com>
2025-09-20 20:38:57 +0600

    Modified defalut color black

--- src/app/layout.js
+++ src/app/layout.js
@@ -24,7 +24,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="bg-background">
+        <div>
           <Navbar></Navbar>
           <div>
             {children}
