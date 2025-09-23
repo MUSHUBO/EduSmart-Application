defe40c6cdf694b97492582d5a200accfa1818f2
MUSHUBO <shubo3132@gmail.com>
2025-09-23 12:26:26 +0600

    Modifies in cotact page and about page some padding margin etc

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -5,7 +5,7 @@ import StudentFrom from "@/components2/StudentFrom";
 
 export default function ContactPage() {
   return (
-    <section className="bg-background py-12 px-4 md:px-10">
+    <section className="bg-background py-8 lg:py-16 w-11/12 mx-auto">
       <div className="container mx-auto">
         <ContactInfo></ContactInfo>
         <StudentFrom></StudentFrom>
