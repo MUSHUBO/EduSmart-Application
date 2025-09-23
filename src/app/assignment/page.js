01f9b45b7d4de2e6db291c1be264711fa9e05214
rkmehedi <astermehedi@gmail.com>
2025-09-24 02:56:33 +0600

    added a Assignment Dashboard Basic

---
+++ src/app/assignment/page.js
@@ -0,0 +1,14 @@
+import CreateAssignmentForm from "../components/assignment/CreateAssignmentForm";
+import AssignmentBoard from "./AssignmentBoard";
+
+export default function NewAssignmentPage() {
+    return (
+        <div className="bg-background min-h-screen py-12">
+            <main className="container mx-auto px-4">
+                <AssignmentBoard />
+                <CreateAssignmentForm />
+               
+            </main>
+        </div>
+    );
+}
