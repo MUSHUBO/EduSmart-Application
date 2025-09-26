160c8a7001f114f746c91864f4be6784accbf8fb
Sabbir-75 <mdsabbirhossain9200@gmail.comm>
2025-09-26 18:42:59 +0600

    Marketing section update

--- src/components/sections/Testimonials/Testimonials.jsx
+++ src/components/sections/Testimonials/Testimonials.jsx
@@ -29,44 +29,6 @@ const NextArrow = ({ onClick }) => (
    </button>
 );
 
-// const testimonials = [
-//    {
-//       id: 1,
-//       name: "Ahsan Rahman",
-//       photo: "https://randomuser.me/api/portraits/men/32.jpg",
-//       rating: 5,
-//       description: "EduSmart school management system makes our school's operation efficient. Highly recommended!",
-//    },
-//    {
-//       id: 2,
-//       name: "Nusrat Jahan",
-//       photo: "https://randomuser.me/api/portraits/women/44.jpg",
-//       rating: 5,
-//       description: "Managing students and teachers has never been easier. eduSmart is a lifesaver for our administration team.",
-//    },
-//    {
-//       id: 3,
-//       name: "Imran Hossain",
-//       photo: "https://randomuser.me/api/portraits/men/78.jpg",
-//       rating: 5,
-//       description: "From student management to report generation, this does it all efficiently. Truly a for school management.",
-//    },
-//    {
-//       id: 4,
-//       name: "Shabnur Akter",
-//       photo: "https://randomuser.me/api/portraits/women/65.jpg",
-//       rating: 5,
-//       description: "Our teachers love how simple it is to track attendance and grades. this is a perfect solution for modern schools.",
-//    },
-//    {
-//       id: 5,
-//       name: "Rafiq Ahmed",
-//       photo: "https://randomuser.me/api/portraits/men/56.jpg",
-//       rating: 5,
-//       description: "The dashboard is very intuitive and user-friendly. this helps us save time and avoid confusion in daily tasks.",
-//    }
-// ];
-
 export const testimonials = [
    {
       id: 1,
@@ -184,26 +146,6 @@ function Testimonials() {
 
 
             {testimonials.map((t) => (
-               // <div key={t.id} className="flex h-full">
-               //    <TestimonialsCard>
-
-
-               //       <img
-               //          src={t.photo}
-               //          alt={t.name}
-               //          className="w-24 h-24 rounded-full object-cover border-4 border-white shadow-lg"
-               //       />
-               //       <h3 className="mt-4 text-lg text-popover dark:text-popover font-semibold">{t.name}</h3>
-               //       <div className="flex mt-2">
-               //          {Array.from({ length: t.rating }).map((_, i) => (
-               //             <svg key={i} className="w-5 h-5 text-yellow-400" fill="currentColor" viewBox="0 0 20 20">
-               //                <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.286 3.958a1 1 0 0 0 .95.69h4.162c.969 0 1.371 1.24.588 1.81l-3.37 2.448a1 1 0 0 0-.364 1.118l1.287 3.958c.3.921-.755 1.688-1.54 1.118l-3.37-2.448a1 1 0 0 0-1.176 0l-3.37 2.448c-.784.57-1.838-.197-1.539-1.118l1.287-3.958a1 1 0 0 0-.364-1.118L2.063 9.385c-.783-.57-.38-1.81.588-1.81h4.162a1 1 0 0 0 .95-.69l1.286-3.958z" />
-               //             </svg>
-               //          ))}
-               //       </div>
-               //       <p className="mt-4 text-popover-foreground dark:text-popover-foreground">{t.description}</p>
-               //    </TestimonialsCard>
-               // </div>
                <div className="bg-muted mr-12 max-h-[400px] rounded-xl shadow-md p-6 relative">
                   {/* Quote Text */}
                   <p className="text-gray-600 text-lg leading-relaxed mb-6">
