e7c75c5e6ba27c2c055b958820035585f706ba2f
MUSHUBO <shubo3132@gmail.com>
2025-09-26 19:17:26 +0600

    Added some change

--- src/components/page/Navbar/Navbar.jsx
+++ src/components/page/Navbar/Navbar.jsx
@@ -69,6 +69,7 @@ const Navbar = () => {
       }
     };
 
+    
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
