954e94614d3285877e2e345c61c1e0bf5e0a831e
Sabbir-75 <mdsabbirhossain9200@gmail.comm>
2025-09-30 22:23:22 +0600

    our blog section implementation complete

--- src/components/sections/Bolgs/Blogs.jsx
+++ src/components/sections/Bolgs/Blogs.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { CgProfile } from "react-icons/cg";
 import { FaRegComments } from "react-icons/fa";
 import { FaLongArrowAltRight } from "react-icons/fa";
+import { SlCalender } from "react-icons/sl";
 
 const Blogs = () => {
     const articles = [
@@ -22,7 +23,8 @@ const Blogs = () => {
             author: "Michael Johnson",
             comments: 5,
             title: "Learning Never Exhausts the Mind, Explore New Knowledge Daily.",
-            link: "#"
+            link: "#",
+            category: "Learning"
         },
         {
             id: 3,
@@ -45,95 +47,123 @@ const Blogs = () => {
                     It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.
                 </p>
             </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
-                <div className="max-w-[380px] overflow-hidden rounded-2xl shadow-md hover:shadow-lg transition-shadow">
-                    {/* Image */}
-                    <div className="relative w-full">
-                        <img
-                            src="https://i.ibb.co.com/Hk7dxhd/01.jpg"
-                            alt="https://i.ibb.co.com/Hk7dxhd/01.jpg"
-                            className="h-full w-full object-cover"
-                        />
-                    </div>
-                    <div className='px-4'>
+            <div className='flex flex-col lg:flex-row justify-center gap-6'>
+                <div className="relative p-2">
+                    <div className='bg-muted dark:bg-muted max-w-[380px] mx-auto group overflow-hidden rounded-2xl shadow-md hover:shadow-lg transition-shadow'>
 
 
-                        <div className='flex justify-between border-b-2 border-secondary/20 py-4'>
-                            <div className='flex items-center gap-2 cursor-pointer'>
-                                <CgProfile className='text-secondary' size={20} />
-                                <h1 className='text-base font-medium hover:text-secondary duration-200 '>Alicia Davis</h1>
+                        {/* Date */}
+                        <div className=' absolute top-10 md:-right-1 right-0 z-10 bg-secondary max-w-40 text-[#FFFFFF] flex justify-center items-center gap-2 cursor-pointer py-1 px-2 rounded-md'>
+                            <SlCalender size={18} />
+                            <h1 className='text-sm font-medium hover:text-secondary duration-300 '>June 18, 2025</h1>
+                        </div>
+                        {/* Image */}
+                        <div className="relative w-full overflow-hidden">
+                            <img
+                                src="https://i.ibb.co.com/Hk7dxhd/01.jpg"
+                                alt="https://i.ibb.co.com/Hk7dxhd/01.jpg"
+                                className="h-full w-full object-cover group-hover:scale-110 duration-700"
+                            />
+                            <div className="absolute inset-0 bg-white/50 opacity-30 group-hover:opacity-0 scale-x-0 group-hover:scale-x-150 transition duration-700 ease-initial"></div>
+                        <div className="absolute inset-0 bg-white/50 opacity-30 group-hover:opacity-0 scale-y-0 group-hover:scale-y-150 transition duration-700 ease-initial"></div>
+                        </div>
+                        <div className='px-4'>
+
+
+                            <div className='flex justify-between border-b-2 border-secondary/20 py-4'>
+                                <div className='flex items-center gap-2 cursor-pointer'>
+                                    <CgProfile className='text-secondary' size={20} />
+                                    <h1 className='text-base font-medium hover:text-secondary duration-300 '>Alicia Davis</h1>
+                                </div>
+                                <div className='flex items-center gap-2 cursor-pointer'>
+                                    <FaRegComments className='text-secondary' size={20} />
+                                    <h1 className='text-base font-medium hover:text-secondary duration-300 '>03 Comments</h1>
+
+                                </div>
                             </div>
-                            <div className='flex items-center gap-2 cursor-pointer'>
-                                <FaRegComments className='text-secondary' size={20} />
-                                <h1 className='text-base font-medium hover:text-secondary duration-200 '>03 Comments</h1>
 
+                            <div>
+                                <h3 className="text-xl font-bold mt-3 hover:text-secondary duration-300 cursor-pointer">There Are Many Variations Passage Have Suffered Available.</h3>
+                            </div>
+                            <div className='py-6'>
+                                <Link href={"/"} className="relative inline-flex items-center rounded-lg justify-center px-3 py-3 md:px-4 md:py-2 lg:px-5 lg:py-2.5 overflow-hidden font-mono font-medium tracking-tighter text-white bg-secondary group">
+                                    <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-primary rounded-full group-hover:w-56 group-hover:h-56"></span>
+                                    <span className="group-hover:text-[#FFFFFF] relative flex text-sm md:text-base duration-500 lg:text-md font-semibold items-center gap-3">
+                                        Read More <FaLongArrowAltRight />
+                                    </span>
+                                </Link>
                             </div>
                         </div>
+                    </div>
+                </div>
 
+                <div className="max-w-[380px] mx-auto group relative overflow-hidden rounded-2xl shadow-md hover:shadow-lg transition-shadow">
+                    {/* Image */}
+                    <div className="relative w-full h-full overflow-hidden">
+                        <img
+                            src="https://i.ibb.co.com/LhSwk2jC/blog-07.jpg"
+                            alt="Blog thumbnail"
+                            className="h-full w-full object-cover transform duration-700 group-hover:scale-110"
+                        />
+
+                        {/* White overlay shade */}
+                        <div className="absolute inset-0 bg-white/50 opacity-30 group-hover:opacity-0 scale-x-0 group-hover:scale-x-150 transition duration-700 ease-initial"></div>
+                        <div className="absolute inset-0 bg-white/50 opacity-30 group-hover:opacity-0 scale-y-0 group-hover:scale-y-150 transition duration-700 ease-initial"></div>
+                    </div>
+                </div>
+
+
+                <div className="max-w-[380px] mx-auto rounded-2xl shadow-md hover:shadow-lg transition-shadow flex flex-col justify-between">
+                    <div className="bg-muted group dark:bg-muted p-5 space-y-2 shadow-md hover:shadow-lg transition-shadow overflow-hidden">
                         <div>
-                            <h3 className="text-xl font-bold mt-3">There Are Many Variations Passage Have Suffered Available.</h3>
-                        </div>
-                        <div className='py-6'>
-                            <Link href={"/"} className="relative inline-flex items-center rounded-lg justify-center px-3 py-3 md:px-4 md:py-2 lg:px-5 lg:py-2.5 overflow-hidden font-mono font-medium tracking-tighter text-white bg-secondary group">
-                    <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-primary rounded-full group-hover:w-56 group-hover:h-56"></span>
-                    <span className="group-hover:text-[#FFFFFF] relative flex text-sm md:text-base duration-300 lg:text-md font-semibold items-center gap-3">
-                       Read More <FaLongArrowAltRight />
-                    </span>
-                </Link>
-                        </div>
-                    </div>
-                </div>
-                <div className="max-w-md overflow-hidden rounded-2xl shadow-md hover:shadow-lg transition-shadow">
-                    {/* Image */}
-                    <div className="relative h-76 w-full">
-                        <img
-                            src="https://i.ibb.co.com/Hk7dxhd/01.jpg"
-                            alt="https://i.ibb.co.com/Hk7dxhd/01.jpg"
-                            className="h-full w-full object-cover"
-                        />
-                    </div>
+                            {/* Category Tag */}
+                            <Link href={"/"} className="relative inline-flex items-center justify-center px-3 py-3 md:px-4 md:py-2 lg:px-5 lg:py-2.5 overflow-hidden font-mono font-medium tracking-tighter text-white bg-secondary group">
+                                <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-primary rounded-full group-hover:w-56 group-hover:h-56"></span>
+                                <span className="group-hover:text-[#FFFFFF] relative flex text-sm md:text-base duration-500 lg:text-md font-semibold items-center gap-3">
+                                    Learning
+                                </span>
+                            </Link>
 
-                    <div className='flex justify-between'>
-                        <div className='flex items-center gap-1.5'>
-                            <CgProfile />
-                            Alicia Davis
+                            {/* Title */}
+                            <h3 className="text-xl font-bold hover:text-secondary duration-300 cursor-pointer border-b-2 border-secondary/20 py-4">Learning Never Exhausts the Mind, Explore New Knowledge Daily.</h3>
                         </div>
-                        <div className='flex items-center gap-1.5'>
-                            <FaRegComments />
-                            03 Comments
+
+                        {/* Footer */}
+                        <div className="flex items-center justify-between py-3">
+                            <a
+                                href="/"
+                                className="text-base font-medium hover:text-secondary duration-300 "
+                            >
+                                READ MORE
+                            </a>
+                            <span className="text-base text-popover-foreground dark:text-popover-foreground font-semibold">July 02, 2025</span>
                         </div>
                     </div>
+                    <div className="bg-muted group dark:bg-muted space-y-2 p-5 shadow-md hover:shadow-lg transition-shadow overflow-hidden">
+                        <div>
+                            {/* Category Tag */}
+                            <Link href={"/"} className="relative inline-flex items-center justify-center px-3 py-3 md:px-4 md:py-2 lg:px-5 lg:py-2.5 overflow-hidden font-mono font-medium tracking-tighter text-white bg-secondary group">
+                                <span className="absolute w-0 h-0 transition-all duration-500 ease-out bg-primary rounded-full group-hover:w-56 group-hover:h-56"></span>
+                                <span className="group-hover:text-[#FFFFFF] relative flex text-sm md:text-base duration-500 lg:text-md font-semibold items-center gap-3">
+                                    Success
+                                </span>
+                            </Link>
 
-                    <div className="p-4">
-                        <h3 className="text-lg font-bold mt-3">There Are Many Variations Passage Have Suffered Available.</h3>
-                    </div>
-
-                </div>
-                <div className="max-w-md overflow-hidden rounded-2xl shadow-md hover:shadow-lg transition-shadow">
-                    {/* Image */}
-                    <div className="relative h-76 w-full">
-                        <img
-                            src="https://i.ibb.co.com/Hk7dxhd/01.jpg"
-                            alt="https://i.ibb.co.com/Hk7dxhd/01.jpg"
-                            className="h-full w-full object-cover"
-                        />
-                    </div>
-
-                    <div className='flex justify-between'>
-                        <div className='flex items-center gap-1.5'>
-                            <CgProfile />
-                            Alicia Davis
+                            {/* Title */}
+                            <h3 className="text-xl font-bold hover:text-secondary duration-300 cursor-pointer border-b-2 border-secondary/20 py-4">Collaboration and Teamwork Are the Keys to Growth and Success.</h3>
                         </div>
-                        <div className='flex items-center gap-1.5'>
-                            <FaRegComments />
-                            03 Comments
+
+                        {/* Footer */}
+                        <div className="flex items-center justify-between py-3">
+                            <a
+                                href="/"
+                                className="text-base font-medium hover:text-secondary duration-300 "
+                            >
+                                READ MORE
+                            </a>
+                            <span className="text-base text-popover-foreground dark:text-popover-foreground font-semibold">August 12, 2025</span>
                         </div>
                     </div>
-
-                    <div className="p-4">
-                        <h3 className="text-lg font-bold mt-3">There Are Many Variations Passage Have Suffered Available.</h3>
-                    </div>
-
                 </div>
             </div>
         </section>
