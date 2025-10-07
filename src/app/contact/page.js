0f63fd3231e3ed07fb40e77e76dbaf37507f5192
riy8n <mahafujhossainriyan02@gmail.com>
2025-10-07 11:38:51 +0600

    remove the translate oage from contact us page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,7 +2,6 @@ import ContactBanner from "@/components2/contact/ContacrBanner";
 import ContactSection from "@/components2/contact/ContactSection";
 import OurLocation from "@/components2/contact/OurLocation";
 import SendMassage from "@/components2/contact/SendMassage";
-import GoogleTranslate from "@/components2/GoogleTranslate";
 
 
 
@@ -17,7 +16,7 @@ export default function ContactPage() {
     <ContactSection></ContactSection>
      <SendMassage></SendMassage>
       <OurLocation></OurLocation> 
-     <GoogleTranslate></GoogleTranslate>
+ 
       </div>
       
     </section>
