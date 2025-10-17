3364f24edf0ddcdb6340036f06b2c65a3f2c9480
MUSHUBO <shubo3132@gmail.com>
2025-10-17 11:55:22 +0600

    FIX Deployment Errors in Login, Register and Contect page.

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -4,21 +4,21 @@ import OurLocation from "@/components2/contact/OurLocation";
 import SendMassage from "@/components2/contact/SendMassage";
 
 
-
-
-
 export default function ContactPage() {
   return (
-    <section className=" mx-auto py-8 md:py-12 lg:py-16">
+    <section className=" mx-auto">
       <ContactBanner></ContactBanner>
       <div className="container mx-auto space-y-2">
 
-    <ContactSection></ContactSection>
-     <SendMassage></SendMassage>
-      <OurLocation></OurLocation> 
- 
+        <ContactSection></ContactSection>
+        <SendMassage></SendMassage>
+        <OurLocation></OurLocation>
+
       </div>
-      
+
     </section>
   );
 }
+
+// Prevent Turbopack from prerendering this page
+export const dynamic = 'force-dynamic';
