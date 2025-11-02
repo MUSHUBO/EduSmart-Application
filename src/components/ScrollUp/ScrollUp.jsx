15d7845810f4ac35cf9f22851675ff87ed295e32
MUSHUBO <shubo3132@gmail.com>
2025-11-02 22:20:31 +0600

    FIX Responsivenes on scroolup button

--- src/components/ScrollUp/ScrollUp.jsx
+++ src/components/ScrollUp/ScrollUp.jsx
@@ -28,25 +28,18 @@ const ScrollUp = () => {
     }, []);
 
     return (
-        <>
+        <div className="hidden md:flex">
             {isVisible && (
                 <button
                     onClick={scrollToTop}
-                    className="fixed bottom-6 right-3 sm:bottom-12 sm:right-6
-                     w-10 h-10 md:w-11 md:h-11
-                     bg-primary text-muted hover:text-primary border-2 border-primary hover:bg-muted
-                     rounded-md flex justify-center items-center transform -rotate-90 hover:-translate-y-1
-                     duration-300 ease-in-out cursor-pointer z-50"
+                    className="fixed bottom-12 right-5 w-11 h-11 bg-primary text-muted hover:text-primary border-2 border-primary hover:bg-muted rounded-md justify-center items-center transform -rotate-90 hover:-translate-y-1 duration-500 ease-in-out cursor-pointer"
                 >
-                    <MdOutlineDoubleArrow
-                        size={36}
-                        className="w-8 h-8 md:w-10 md:h-10"
-                    />
+                    <MdOutlineDoubleArrow size={36} />
                 </button>
             )}
 
             <audio ref={audioRef} src="/audio/back-to-top.mp3" />
-        </>
+        </div>
     );
 };
 
